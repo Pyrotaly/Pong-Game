f58048da4559eb5eb7db708ac9485dfe896d7b52
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-06-12 20:39:47 -0400

    Add Empty Project

---
+++ Pong/Assets/Paddle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float _movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
